65c67abcc951e46d90768ed819fa17fff2a78d4f
dicaros <stedicaro-1@yahoo.it>
2019-12-30 09:17:35 +0100

    Recovered Userdetails.js file

---
+++ webApp/src/components/UserDetails.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import userpic from '../img/userfotodefault.jpg';
+
+var countpage = 0 + ' '
+
+class UserDetails extends React.Component {
+
+   render() {      
+
+        const Table1 = () => {
+           return(        
+              this.props.useritems._embedded.myUserDetails.map ((row) =>                   
+                   {
+                     if(row.myuser.name == this.props.username)
+                     {
+                        let udetails = [row.playedcount, row.noShowCount, row.lastName, row.phone, 'n/a'];
+                        return(
+                          <table className = 'tablelist2'>
+                              <thead>
+                                  <tr key='001'>
+                                      <th className = 'userdetailsheader' height='120px'><center><img src={userpic} className='userpic' alt='O'></img></center></th>
+                                  </tr>
+                                  <tr key='002'>
+                                      <th className = 'userdetailsheader' height='30px'>{row.firstName} {row.lastName}
+                                      <h4 className = 'userdetailsheader'>({row.myuser.email})</h4>
+                                      <h4 className = 'userdetailsheader'>Member since: n/a</h4>
+                                      </th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                <tr key='0'>
+                                  <td>
+                                      <table className='userdetails'>
+
+                                            <tbody>
+                                              <tr key='1'>
+                                                    <td height = '15px' colSpan='2'> </td>
+                                                </tr>
+                                                <tr  key='2' height='30px'>
+                                                    <td className = 'userlist'>Games Played</td>
+                                                    <td className = 'userlist'>{row.playedcount}</td>
+                                                </tr>
+                                                <tr key='3' height='30px'>
+                                                    <td className = 'userlist'>Games Missed</td>
+                                                    <td className = 'userlist'>{row.noShowCount}</td>
+                                                </tr>
+                                                <tr  key='4' height='30px'>
+                                                    <td className = 'userlist'>Phone number</td>
+                                                    <td className = 'userlist'>{row.phone}</td>
+                                                </tr>
+                                                <tr key='5' height='30px'>
+                                                    <td className = 'userlist'>Email</td>
+                                                    <td className = 'userlist'>{row.myuser.email}</td>
+                                                </tr>
+                                  </tbody>
+                                          </table>
+                                    </td>
+                                  </tr>
+                               </tbody>
+                               <tfoot>
+                                   <tr key='003'>
+                                      <td></td>                             
+                                   </tr>
+                              </tfoot>
+                          </table>
+                  )}
+               })
+           )
+         }
+                           
+         if(this.props.loginsuccess && typeof this.props.useritems._embedded != 'undefined' && this.props.navigate == 'games') {
+          return (
+                    <Table1 />            
+                 );
+         }
+         else {
+          return ''
+        }
+     }
+ }
+ 
+export default UserDetails;
