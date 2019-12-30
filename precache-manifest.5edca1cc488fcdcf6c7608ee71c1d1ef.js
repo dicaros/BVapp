0d6310ade93e55ac822095ab8a23f398d68f0b44
dicaros <stedicaro-1@yahoo.it>
2019-12-30 10:18:24 +0100

    Updates

---
+++ precache-manifest.5edca1cc488fcdcf6c7608ee71c1d1ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3902caed250bcb22fba4ab07986c79fb",
+    "url": "/bvapp/index.html"
+  },
+  {
+    "revision": "99e6634ef85b1a7d9316",
+    "url": "/bvapp/static/css/main.f742636c.chunk.css"
+  },
+  {
+    "revision": "e7ff82cde99c37a4a6b5",
+    "url": "/bvapp/static/js/2.c8c01142.chunk.js"
+  },
+  {
+    "revision": "99e6634ef85b1a7d9316",
+    "url": "/bvapp/static/js/main.c5538f52.chunk.js"
+  },
+  {
+    "revision": "3b0fdababb2632e6caf8",
+    "url": "/bvapp/static/js/runtime-main.17271295.js"
+  }
+]);
