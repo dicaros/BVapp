19777e037038efaf79b217088d0ed863750104d8
dicaros <stedicaro-1@yahoo.it>
2020-01-06 19:04:04 +0100

    minor changes

--- webApp/src/constants/constants.js
+++ webApp/src/constants/constants.js
@@ -1,12 +1,12 @@
 
 // IP local - Praha
-export const url = 'http://192.168.0.103:8080/'
+//export const url = 'http://192.168.0.103:8080/'
 
 // IP local - Bosovice
 //export const url = 'http://192.168.1.106:8080/'
 
 // local
-//export const url = 'http://localhost:8080/'
+export const url = 'http://localhost:8080/'
 
 // AWS
 //export const url = 'http://18.197.145.113:8080/'
